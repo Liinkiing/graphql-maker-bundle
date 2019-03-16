705753e3d1899fcf8dae79158583a35af56f2a8d
Omar Jbara <omar.Jbara2@gmail.com>
2019-03-16 22:53:02 +0100

    - Removed space after comment to easily uncomment lines while persisting YAML indentation

--- src/Resources/skeleton/Query.fragment.tpl.php
+++ src/Resources/skeleton/Query.fragment.tpl.php
@@ -3,11 +3,11 @@
 <?php if ($type) { ?>
         type: '<?= $type ?><?= ($nullable === true) ? '' : '!' ?>'<?= "\n" ?>
 <?php } ?>
-        # If you want to use a Relay connection<?= "\n" ?>
-        # argsBuilder: Relay::Connection<?= "\n" ?>
-        # resolve: '@=resolver("<?= $rootNamespace ?>\\Resolver\\Query\\<?= ucfirst($name).'Resolver' ?>", [args])'<?= "\n" ?>
-        # args:<?= "\n" ?>
-        # Put here your custom args for you query (if any)<?= "\n" ?>
-          # first:<?= "\n" ?>
-            # type: Int<?= "\n" ?>
-            # defaultValue: 30<?= "\n" ?>
+        #If you want to use a Relay connection<?= "\n" ?>
+        #argsBuilder: Relay::Connection<?= "\n" ?>
+        #resolve: '@=resolver("<?= $rootNamespace ?>\\Resolver\\Query\\<?= ucfirst($name).'Resolver' ?>", [args])'<?= "\n" ?>
+        #args:<?= "\n" ?>
+        #Put here your custom args for you query (if any)<?= "\n" ?>
+          #first:<?= "\n" ?>
+            #type: Int<?= "\n" ?>
+            #defaultValue: 30<?= "\n" ?>
